5692114dbc0a2a26f5ca5431f8c7fd1ff72e5cc8
Rubén <rjulian@elpuig.xeill.net>
2025-05-15 19:14:34 +0200

    route logo

--- front-react/src/components/Header.tsx
+++ front-react/src/components/Header.tsx
@@ -5,7 +5,7 @@ import AuthButtons from './LoginBtns';
 const Header: React.FC = () => (
   <header className="header" style={{ display: "grid", gridTemplateColumns: "auto auto", backgroundColor: "#1D1D1D" }}>
     <div className="header-left">
-      <img src="./logo/ReliquarySanctuaryLogo.svg" alt="logo" style={{height:"8rem"}} />
+      <img src="/logo/ReliquarySanctuaryLogo.svg" alt="logo" style={{height:"8rem"}} />
       <h1 className="logo">
         <span style={{ display: 'block' }}>COMMANDER</span>
         <span style={{ display: 'block' }}>RELIQUARY</span>
